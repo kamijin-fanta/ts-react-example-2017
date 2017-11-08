44ece88a8c50ed714c41f650bfcc066cbb761869
kamijin_fanta <kamijin@live.jp>
2017-11-08 19:20:12 +0900

    add i18n

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,15 +1,21 @@
 import * as React from 'react';
 import './Hello.css';
 
+import { Trans, translate, TranslationFunction } from 'react-i18next';
+
 export interface Props {
   name: string;
   enthusiasmLevel?: number;
   onIncrement?: () => void;
   onDecrement?: () => void;
   onDeleyedIncrement?: () => void;
+  t?: TranslationFunction;
 }
 
-function Hello({ name, enthusiasmLevel = 1, onIncrement, onDecrement, onDeleyedIncrement }: Props) {
+function stab<A>(a: A) { return a; }
+
+function Hello(props: Props) {
+  const { name, enthusiasmLevel = 1, onIncrement, onDecrement, onDeleyedIncrement, t = stab} = props;
   if (enthusiasmLevel <= 0) {
     throw new Error('You could be a little more enthusiastic. :D');
   }
@@ -20,6 +26,9 @@ function Hello({ name, enthusiasmLevel = 1, onIncrement, onDecrement, onDeleyedI
         Hello {name + getExclamationMarks(enthusiasmLevel)}
       </div>
       <div>
+        translate: {t('cancel')}
+      </div>
+      <div>
         <button onClick={onDecrement}>-</button>
         <button onClick={onIncrement}>+</button>
         <button onClick={onDeleyedIncrement}>+</button>
@@ -28,7 +37,7 @@ function Hello({ name, enthusiasmLevel = 1, onIncrement, onDecrement, onDeleyedI
   );
 }
 
-export default Hello;
+export default translate()(Hello);
 
 // helpers
 
--- src/index.tsx
+++ src/index.tsx
@@ -7,16 +7,22 @@ import { Provider } from 'react-redux';
 import { Route } from 'react-router';
 import { routerMiddleware, ConnectedRouter } from 'react-router-redux';
 import { history, store } from './store';
+import { I18nextProvider } from 'react-i18next';
+
+import i18n from './i18n';
+
 import './index.css';
 
 ReactDOM.render(
   <Provider store={store}>
-    <ConnectedRouter history={history}  >
-      <div>
-        <Route exact={true} path="/" component={Hello} />
-        <Route path="/about" component={About} />
-      </div>
-    </ConnectedRouter>
+    <I18nextProvider i18n={i18n}>
+      <ConnectedRouter history={history}  >
+        <div>
+          <Route exact={true} path="/" component={Hello} />
+          <Route path="/about" component={About} />
+        </div>
+      </ConnectedRouter>
+    </I18nextProvider>
   </Provider>,
   document.getElementById('root') as HTMLElement
 );
