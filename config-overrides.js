f3aa09d0428efc3c8c6e20acf473bae1d237c09c
kamijin_fanta <kamijin@live.jp>
2017-12-14 20:49:18 +0900

    add plugin s2s-typed-react-router

--- config-overrides.js
+++ config-overrides.js
@@ -2,7 +2,18 @@ const paths = require('react-scripts-ts/config/paths');
 
 module.exports = {
   jest: function(config) {
+    // do not use tsconfig.test.json at test time
     config.globals['ts-jest']['tsConfigFile'] = paths.appTsConfig;
+
+    // include JS files for testing
+    config.testMatch.push('<rootDir>/src/**/?(*.)(spec|test).js');
+    config.transform['^.+\\.js$'] = 'babel-jest';
+
+    // ignore watch
+    config.watchPathIgnorePatterns = [
+      '.*\/node_modules\/.*',
+    ];
+
     return config;
   },
 };
---
+++ src/plugins/s2s-typed-react-router.test.js
@@ -0,0 +1,32 @@
+import plugin from './s2s-typed-react-router';
+import pluginTester from 'babel-plugin-tester';
+
+pluginTester({
+  plugin,
+  pluginOptions: {
+  },
+  snapshot: true,
+  tests: [
+    {
+      title: 'single',
+      code: `
+        import { Path ,} from './lib';
+        
+        export const Routes: RoutesType = {
+          Hello: Path({ url: '/', title: 'Top Page', component: Hello }),
+          TodoView: Path({ url: '/todo/view/:page?', title: 'view todo', component: TodoView }),
+          TodoEdit: Path({ url: '/todo/edit/:id', title: 'edit todo', component: TodoEdit }),
+          TodoAdd: Path({ url: '/todo/add/:parent/:genre?', title: 'add todo', component: TodoAdd }),
+        };
+
+        export interface RoutesType {
+          Hello: RoutePath<HelloRouteProps, EmptyParameterProps>;
+          TodoView: RoutePath<TodoViewRouteProps, OptionalParameterProps<TodoViewRouteProps>>;
+          TodoEdit: RoutePath<TodoEditRouteProps, ParameterProps<TodoEditRouteProps>>;
+        }
+        export interface HelloRouteProps {}
+        interface TodoViewRouteProps {}
+      `,
+    },
+  ],
+});
